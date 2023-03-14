baac73e108f1dd700dd49ced83f9ff69d787bfe9
tdetugny <tdetugny@clever-age.com>
2023-03-14 10:23:32 +0100

    feat: init project

---
+++ crop-and-clean-svg.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+
+# INSTALL
+# 1- install svgo script read https://github.com/svg/svgo
+# 2- install inkscape https://inkscape.org/fr/
+# create 2 folders "input" and "output" with good permissions
+# place all svg files inside the folder "input"
+# run this script
+# get your new svg files inside the folder output
+
+INPUT="./input"
+TARGET="./output"
+
+if [ ! -d "$INPUT" ]; then
+	echo "input directory \"$INPUT \" does not exist."
+  exit
+fi
+
+if [ ! -d "TARGET" ]; then
+  echo "$TARGET";
+  mkdir -p "$TARGET";
+fi;
+
+
+clean(){
+  OUTPUT_FILE="$1"
+  echo "======>$OUTPUT_FILE"
+  exec svgo -i "$OUTPUT_FILE" --config config.js
+}
+export -f clean;
+
+crop(){
+  INPUT_FILE="$1"
+  OUTPUT_FILE="$2"
+  exec inkscape --actions="export-area-drawing;export-margin:0.1;export-type:svg;export-plain-svg; export-do" $INPUT_FILE  --export-filename="$OUTPUT_FILE" &
+  sleep 1;
+}
+export -f crop;
+
+process(){
+  INPUT_FILE="$1"
+  FILE_NAME="$(basename "$INPUT_FILE")"
+  TARGET="$2"
+
+  OUTPUT_FILE="$TARGET/$FILE_NAME";
+  crop "$INPUT_FILE" "$OUTPUT_FILE";
+
+  clean "$OUTPUT_FILE"
+  echo "END"
+}
+export -f process
+
+find $INPUT -maxdepth 1 -mindepth 1 -type f -name *.svg -exec bash -c 'process "$0" '"$TARGET" {} \;
+
